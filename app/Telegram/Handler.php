813632f9989a5c76085e1bee4a717fed565e14c9
Andrey <garry20121@gmail.com>
2025-01-31 19:34:31 +0300

    Revert "off macd"
    This reverts commit 8cb73d9f8ce4a1b8dbd7e1060dfd887234a7bf35.

--- app/Telegram/Handler.php
+++ app/Telegram/Handler.php
@@ -23,12 +23,12 @@ class Handler extends WebhookHandler
             $RSI = $query->{'RSI|240'};
             $MACD_macd = $query->{'MACD.macd|240'};
             $MACD_signal = $query->{'MACD.signal|240'};
-            if ($RSI >= 25 && $RSI <= 30) {
+            if (($RSI >= 25 && $RSI <= 30) && ($MACD_macd >= $MACD_signal)) {
                 foreach ($chats as $chat) {
                     $chat->html("<b>Сигнал на покупку</b>\n$stock->name\n$stock->symbol\nRSI: $RSI\nMACD (синяя): $MACD_macd\nMACD (оранжевая): $MACD_signal\nhttps://www.tbank.ru/invest/stocks/$stock->symbol?utm_source=security_share")->send();
                 }
             }
-            elseif ($RSI >= 65 && $RSI <= 80) {
+            elseif (($RSI >= 65 && $RSI <= 80) && ($MACD_macd <= $MACD_signal)) {
                 foreach ($chats as $chat) {
                     $chat->html("<b>Сигнал на продажу</b>\n$stock->name\n$stock->symbol\nRSI: $RSI\nMACD (синяя): $MACD_macd\nMACD (оранжевая): $MACD_signal\nhttps://www.tbank.ru/invest/stocks/$stock->symbol?utm_source=security_share")->send();
                 }
